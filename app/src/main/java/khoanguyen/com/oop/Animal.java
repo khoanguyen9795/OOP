3df731ec3ab683eb02895e1b2f2e336b9cb04486
khoanguyen9795 <khoanguyen438@gmail.com>
2020-04-24 20:14:49 +0700

    class and object

---
+++ app/src/main/java/khoanguyen/com/oop/Animal.java
@@ -0,0 +1,6 @@
+package khoanguyen.com.oop;
+
+public class Animal {
+    String name;
+    int weight;
+}
---
+++ app/src/main/java/khoanguyen/com/oop/MainActivity.java
@@ -0,0 +1,15 @@
+package khoanguyen.com.oop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Animal meo = new Animal();
+    }
+}
